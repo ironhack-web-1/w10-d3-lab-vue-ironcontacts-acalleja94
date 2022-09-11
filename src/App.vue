ee316fa2c42db8ea550f093596955b6e89307bee
A. Calleja <albacalpa@gmail.com>
2022-09-11 13:49:39 +0200

    now it takes the data from de json directly

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 
 
 <template>
-  <h1 class="display-1">IronContacts</h1>
+  <h1 class="display-1 text-primary">IronContacts</h1>
 
-<div class="container">
+<div class="container container__pos">
   <table class="table table-hover">
-    <thead class="thead-dark">
+    <thead>
         <tr>
-            <th class="col">Picture</th>
-            <th class="col">Name</th>
-            <th class="col">Popularity</th>
-            <th class="col">Won an Oscar</th>
-            <th class="col">Won an Emmy</th>
+            <th class="col col__style">Picture</th>
+            <th class="col col__style">Name</th>
+            <th class="col col__style">Popularity</th>
+            <th class="col col__style">Won an Oscar</th>
+            <th class="col col__style">Won an Emmy</th>
         </tr>
     </thead>
 
     <tbody>
-        <tr v-for="(element, index) of ListOfContacts" :key="index">
+        <tr v-for="(element, index) of contactSorted" :key="index">
             <td scope="row">
               <img
               :src="element.pictureUrl"
@@ -37,56 +37,12 @@
 
 <script>
   import contacts from "./contacts.json";
+  
   export default {
     data() {
       return {
-        ListOfContacts: [
-          {
-            name: "Idris Elba",
-            pictureUrl:
-              "https://image.tmdb.org/t/p/w500/d9NkfCwczP0TjgrjpF94jF67SK8.jpg",
-            popularity: 11.622713,
-            id: "11731993-0604-4bee-80d5-67ad845d0a38",
-            wonOscar: false,
-            wonEmmy: false,
-          },
-          {
-            name: "Johnny Depp",
-            pictureUrl:
-              "https://image.tmdb.org/t/p/w500/kbWValANhZI8rbWZXximXuMN4UN.jpg",
-            popularity: 15.656534,
-            id: "7dad00f7-3949-477d-a7e2-1467fd2cfc06",
-            wonOscar: false,
-            wonEmmy: false,
-          },
-          {
-            name: "Monica Bellucci",
-            pictureUrl:
-              "https://image.tmdb.org/t/p/w500/qlT4904d8oi2NIs28RrgnIZDFZB.jpg",
-            popularity: 16.096436,
-            id: "0ad5e441-3084-47a1-9e9b-b917539bba71",
-            wonOscar: true,
-            wonEmmy: false,
-          },
-          {
-            name: "Gal Gadot",
-            pictureUrl:
-              "https://image.tmdb.org/t/p/w500/34kHAyBaBhq2kUrmhM15paEBuuI.jpg",
-            popularity: 10.049256,
-            id: "b497e3c4-50bb-4ae2-912f-eb36802c5bc2",
-            wonOscar: false,
-            wonEmmy: true,
-          },
-          {
-            name: "Ian McKellen",
-            pictureUrl:
-              "https://image.tmdb.org/t/p/w500/coWjgMEYJjk2OrNddlXCBm8EIr3.jpg",
-            popularity: 10.070132,
-            id: "0067ae32-97b6-4431-898e-eb1c10150abb",
-            wonOscar: false,
-            wonEmmy: false,
-          },
-        ],
+        contactList: contacts,
+        contactSorted: contacts.slice(0, 5),
       };
     },
     methods: {
@@ -116,10 +72,13 @@
   height: auto;
 }
 
-.container{
-  width: 100%;
-  display: flex;
-  justify-content: center;
+.col__style {
+  text-transform: uppercase;
 }
+
+.container__pos{
+  margin-top: 50px;
+}
+
 </style>
 
